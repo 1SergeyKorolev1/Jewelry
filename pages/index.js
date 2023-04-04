2c6b8416db9b6638b92a33e0c7a212e29c37dbd9
Sergey Korolev <bongrichard@rambler.ru>
2023-04-04 21:18:41 +0300

    add form calculate

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,55 @@
+const footerDate = document.querySelector(".footer-date");
+const inputWork = document.querySelector(".input-work");
+const inputProblem = document.querySelector(".input-problem");
+const inputType = document.querySelector(".input-type");
+const inputWeight = document.querySelector(".input-weight");
+const inputMaterial = document.querySelector(".input-material");
+
 function newData() {
   return new Date().getFullYear();
 }
 
-console.log(newData());
-
-const footerDate = document.querySelector(".footer-date");
 footerDate.textContent = `Sergey Korolev ${newData()}`;
+
+function choiceWork(evt) {
+  if (
+    evt.target.value === "Виды работ" ||
+    Number(evt.target.value) === 3 ||
+    Number(evt.target.value) === 1
+  ) {
+    inputWeight.classList.add("input-disable");
+    inputMaterial.classList.add("input-disable");
+    inputMaterial.value = "Материал изделия";
+    inputType.value = "Вид изделия";
+  }
+
+  if (Number(evt.target.value) === 1 || Number(evt.target.value) === 3) {
+    inputProblem.classList.remove("input-disable");
+  } else {
+    inputProblem.classList.add("input-disable");
+  }
+
+  if (Number(evt.target.value) === 2) {
+    inputType.classList.remove("input-disable");
+  } else {
+    inputType.classList.add("input-disable");
+  }
+}
+
+function choiceProduct(evt) {
+  if (
+    Number(evt.target.value) === 1 ||
+    Number(evt.target.value) === 2 ||
+    Number(evt.target.value) === 3 ||
+    Number(evt.target.value) === 4
+  ) {
+    inputWeight.classList.remove("input-disable");
+    inputMaterial.classList.remove("input-disable");
+  } else {
+    inputWeight.classList.add("input-disable");
+    inputMaterial.classList.add("input-disable");
+  }
+}
+
+inputWork.addEventListener("input", choiceWork);
+inputType.addEventListener("input", choiceProduct);
