29baea536e3074c4e7e9b301038404bdcefd92e8
Sergey Korolev <bongrichard@rambler.ru>
2023-04-05 14:29:32 +0300

    Added the ability to attach a file

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ const inputType = document.querySelector(".input-type");
 const inputWeight = document.querySelector(".input-weight");
 const inputMaterial = document.querySelector(".input-material");
 const inputItemDescription = document.querySelector(".input-item-description");
+const inputFileRepair = document.querySelector(".input-file-repair");
+const inputFileExample = document.querySelector(".input-file-example");
 
 function newData() {
   return new Date().getFullYear();
@@ -22,12 +24,15 @@ function choiceWork(evt) {
     inputMaterial.classList.add("input-disable");
     inputMaterial.value = "Материал изделия";
     inputType.value = "Вид изделия";
+    inputItemDescription.classList.add("input-disable");
   }
 
   if (Number(evt.target.value) === 1 || Number(evt.target.value) === 3) {
     inputProblem.classList.remove("input-disable");
+    inputFileRepair.classList.remove("input-disable");
   } else {
     inputProblem.classList.add("input-disable");
+    inputFileRepair.classList.add("input-disable");
   }
 
   if (Number(evt.target.value) === 2) {
@@ -46,15 +51,18 @@ function choiceProduct(evt) {
   ) {
     inputWeight.classList.remove("input-disable");
     inputMaterial.classList.remove("input-disable");
+    inputFileExample.classList.remove("input-disable");
   } else {
     inputWeight.classList.add("input-disable");
     inputMaterial.classList.add("input-disable");
+    inputFileExample.classList.add("input-disable");
   }
 
   if (Number(evt.target.value) === 5) {
     inputItemDescription.classList.remove("input-disable");
     inputWeight.classList.remove("input-disable");
     inputMaterial.classList.remove("input-disable");
+    inputFileExample.classList.remove("input-disable");
   } else {
     inputItemDescription.classList.add("input-disable");
   }
